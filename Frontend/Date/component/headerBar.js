0c3296b57f6eccafbadb93e2af70cb651db1f6dd
banana970 <84257439+banana970@users.noreply.github.com>
2022-08-21 22:43:18 +0900

    Add files via upload

---
+++ Frontend/Date/component/headerBar.js
@@ -0,0 +1,38 @@
+import { Box, Container, Flex } from "@chakra-ui/react";
+import * as React from "react";
+import { ChevronLeftIcon } from "@chakra-ui/icons";
+import CircleIcon from "./icon";
+import { Text } from "@chakra-ui/react";
+import "../App.css";
+
+function HeaderBar() {
+  return (
+    <Flex
+      flexDirection="row"
+      alignItems="center"
+      justifyContent="space-between"
+      h={"100%"}
+    >
+      <ChevronLeftIcon w="auto" h="25px" marginLeft="16.3" />
+
+      <Text
+        fontFamily={"pretendardVariable"}
+        fontWeight={"bold"}
+        fontSize={"20px"}
+        color={"transparent"}
+        bgClip="text"
+        backgroundImage={"./fontimg.png"}
+        bgPosition={"center"}
+      >
+        2022
+      </Text>
+      <Box marginRight={"29px"}>
+        <CircleIcon boxSize={2} />
+        <CircleIcon boxSize={2} />
+        <CircleIcon boxSize={2} />
+      </Box>
+    </Flex>
+  );
+}
+
+export default HeaderBar;
